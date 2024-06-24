e14b2251472b140c56e95a3b217ef5be34cfe755
linahalifah <linahalifah21@gmail.com>
2024-06-24 18:49:32 +0700

    Add files via upload

---
+++ sertifikasi/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Landing Page - Sertifikasi</title>
+ 
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="#">Sertifikasi</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="lihat.php">data peserta</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="form_pendaftaran.php">daftar sertifikasi</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="login.php">login admin</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="tikomedia.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="LogoBNSP.png" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="tikom.jpg" class="d-block w-100" alt="...">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-6">
+        <h2>Tentang Sertifikasi</h2>
+        <p> Sertifikasi adalah suatu bentuk pengakuan resmi yang diberikan oleh lembaga atau organisasi kepada individu atau entitas setelah berhasil melewati serangkaian pengujian yang mengukur pengetahuan, keterampilan, dan kemampuan dalam suatu bidang tertentu.</p>
+      
+      </div>
+      <div class="col-md-6">
+        <img src="logotikom.jpg" class="img-fluid" alt="Sertifikasi">
+      </div>
+    </div>
+  </div>
+
+ 
+  <footer class="bg-blue text-white mt-5">
+    <div class="container text-center py-3">
+      
+    </div>
+  </footer>
+
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
